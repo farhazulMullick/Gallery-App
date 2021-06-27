09d09b57879ad1b2709b9241d8a3a56138bbbc79
Farhazul Mullick <farhazulmullick99@gmail.com>
2021-06-27 20:15:40 +0530

    Added intent to go to MainActivity

--- app/src/main/java/com/example/agtgallery/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/agtgallery/splashscreen/SplashScreen.kt
@@ -1,12 +1,25 @@
 package com.example.agtgallery.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.agtgallery.MainActivity
 import com.example.agtgallery.R
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.myLooper()!!).postDelayed(object : Runnable{
+            override fun run() {
+                val intent = Intent(this@SplashScreen, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }, 2000)
     }
 }
