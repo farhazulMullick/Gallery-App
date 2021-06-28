a8526193778725fc1473956c4ce70b174020b79c
Farhazul Mullick <farhazulmullick99@gmail.com>
2021-06-28 19:45:48 +0530

    Added navigation drawer

--- app/src/main/java/com/example/agtgallery/MainActivity.kt
+++ app/src/main/java/com/example/agtgallery/MainActivity.kt
@@ -2,13 +2,43 @@ package com.example.agtgallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.scopes.ActivityScoped
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = drawer_layout
+        navController = this.findNavController(R.id.nav_host_fragment)
+        navigationView = nav_view
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.homeFragment
+        ), drawerLayout)
+
+        navigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, drawerLayout)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp(appBarConfiguration)
     }
 }
